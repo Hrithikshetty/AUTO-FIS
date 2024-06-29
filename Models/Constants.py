3431d2b1cc7b021f8cb52c2ff37777291f41d0c1
Hrithik Shetty <hrithik.cd21@sahyadri.edu.in>
2024-06-29 11:29:08 +0530

    alok you are worst

--- Models/Constants.py
+++ Models/Constants.py
@@ -4,4 +4,4 @@ data_path = 'E:\\Marine_Classification\\Models\\Utilities\\fish_loc.csv'
 
 class_names = ['Bangus', 'Big Head Carp', 'Black Spotted Barb', 'Catfish', 'Climbing Perch', 'Fourfinger Threadfin', 'Fresherwater Eel', 'Glass Perchlet', 'Goby', 'Gold Fish', 'Gourami', 'Grass Crap', 'Green Spotted Puffer', 'Indian Carp', 'Indo Pacific Tarpon', 'Jaguar Fish', 'Janitor Fish', 'Knifefish','Long Snouted Pipefish', 'Mosquito Fish', 'Mudfish', 'Mullet', 'Pangasius', 'Perch', 'Scat Fish', 'Silver Barb', 'Silver Carp', 'Silver Perch', 'Snakehead', 'Tenpounder', 'Tilapia']
 
-api_key = 'sk-proj-cfOXplV7oiPLkxftmzuGT3BlbkFJvAZhRVMUoM0hoTNmoBxy'
+api_key = ''
